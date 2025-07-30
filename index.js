b3ea43d5530f0bdd280d776e8deb5f057317f063
urnisha19 <urnisha09@gmail.com>
2025-07-30 19:43:14 +0600

    edited

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { connectDB } = require("./utils/connectDB");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to MongoDB 
+// Connect to MongoDB
 connectDB();
 
 // Middlewares
 app.use(
-  cors({ origin: "https://eventpick-server.onrender.com", credentials: true })
-); 
+  cors({ origin: "http://localhost:5173/", credentials: true })
+); //Allow requests from frontend running on localhost:5173 with credentials
 app.use(express.json()); // Middleware to parse incoming JSON payloads
 
 // Import and use user-related routes
